71a0d132e67a03ff729fdb58e9324a17518a753c
Tharun kumar <tharunmudaliyarkgf@gmail.com>
2024-02-18 01:44:05 +0530

    static files added

--- food_delivery/settings.py
+++ food_delivery/settings.py
@@ -89,7 +89,13 @@ DATABASES = {
     },
 }
 
-
+SWAGGER_SETTINGS = {
+    'SECURITY_DEFINITIONS': {
+        'basic': {
+            'type': 'basic'
+        }
+    },
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
