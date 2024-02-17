8e9ca9deb749fa74cbe9320274d79face80a2704
Tharun kumar <tharunmudaliyarkgf@gmail.com>
2024-02-18 00:12:37 +0530

    static files added

--- food_delivery/settings.py
+++ food_delivery/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
+from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -128,9 +129,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
 
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+# Adjust STATIC_ROOT for deployment URL
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
