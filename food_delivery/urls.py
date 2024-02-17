0c8125f5173e549a23f69a20df7a78839d986a0e
Tharun kumar <tharunmudaliyarkgf@gmail.com>
2024-02-18 00:50:42 +0530

    sstatic files added

--- food_delivery/urls.py
+++ food_delivery/urls.py
@@ -4,6 +4,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('pricing/', include('pricing.urls')),
-]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
 
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
